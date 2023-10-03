2f8b0b41bc5b381eded5b49f525e96cd8c84de3d
AndreRibeiro07 <an_dre_07@hotmail.com>
2023-10-03 16:02:44 -0300

    concluindo o desafio do herói da DIO

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("olá mundão");
+let nomeHeroi = "Californication"
+let expHeroi = 7777777
+let rankFerro = "Ferro"
+let rankBronze = "Bronze"
+let rankPrata = "Prata"
+let rankOuro = "Ouro"
+let rankPlatina = "Platina"
+let rankAscendente = "Ascendente"
+let rankImortal = "Imortal"
+let rankRadiante = "Radiante"
+
+if (expHeroi < 1000){
+    
+    console.log(" O personagem " + nomeHeroi + " tem : " + expHeroi + " de experiência e está no rank : " + rankFerro)
+
+} if (expHeroi >= 1001 && expHeroi <= 2000 ) {
+    console.log(" O personagem " + nomeHeroi + " tem : " + expHeroi + " de experiência e está no rank : " + rankBronze)
+
+} if (expHeroi >= 2001 && expHeroi <= 6000 ) {
+    console.log(" O personagem " + nomeHeroi + " tem : " + expHeroi + " de experiência e está no rank : " + rankPrata )
+
+} if (expHeroi >= 6001 && expHeroi <= 7000 ) {
+    console.log(" O personagem " + nomeHeroi + " tem : " + expHeroi + " de experiência e está no rank : " + rankOuro)
+
+} if (expHeroi >= 7001 && expHeroi <= 8000 ) {
+    console.log(" O personagem " + nomeHeroi + " tem : " + expHeroi + " de experiência e está no rank : " + rankPlatina)
+
+} if (expHeroi >= 8001 && expHeroi <= 9000 ) {
+    console.log(" O personagem " + nomeHeroi + " tem : " + expHeroi + " de experiência e está no rank : " + rankAscendente)
+
+} if (expHeroi >= 9001 && expHeroi <= 10000 ) {
+    console.log(" O personagem " + nomeHeroi + " tem : " + expHeroi + " de experiencia e está no rank : " + rankImortal)
+
+} if (expHeroi >= 10001 ) {
+    console.log(" O personagem " + nomeHeroi + " tem : " + expHeroi + " de experiência e está no rank : " + rankRadiante)
+}
+
